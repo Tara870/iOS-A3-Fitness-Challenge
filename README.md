61d598756c9d4c3fdbd25decebc35f3cfc6b8e24
Tara870 <110669177+Tara870@users.noreply.github.com>
2024-05-06 12:34:10 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# iOS-A3-Fitness-Challenge
+ 2024 Autumn iOS Group Task
---
+++ iOS-A3-Fitness Challenge/iOS_A3_Fitness_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_A3_Fitness_ChallengeApp.swift
+//  iOS-A3-Fitness Challenge
+//
+//  Created by Tara Jiang on 6/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_A3_Fitness_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
