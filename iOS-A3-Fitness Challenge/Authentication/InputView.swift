6e7cc0a930ad7e91e9c09dc5ba56c907f1776cd1
Tara870 <110669177+Tara870@users.noreply.github.com>
2024-05-07 23:13:10 +1000

    Reusable InputView added
    a title with a input field
    password secure field *
    normal text field

--- iOS-A3-Fitness Challenge/Authentication/InputView.swift
+++ iOS-A3-Fitness Challenge/Authentication/InputView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+// for login and register
+// reusable code
+// title with a input field
+
 struct InputView: View {
     @Binding var text : String
     let title : String
@@ -14,8 +18,23 @@ struct InputView: View {
     var isSecureField = false
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundColor(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
             
+            // password * / normal text
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
         }
         
         
