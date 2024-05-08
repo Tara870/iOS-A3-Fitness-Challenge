6f1d0335dd193519bdd9b142984fcb114b4b5da2
Tara870 <110669177+Tara870@users.noreply.github.com>
2024-05-09 01:37:08 +1000

    Create user edition 2
    Main Actor
    Init function
    configure firebase in app

--- iOS-A3-Fitness Challenge/Authentication/AuthViewModel.swift
+++ iOS-A3-Fitness Challenge/Authentication/AuthViewModel.swift
@@ -9,12 +9,15 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
+@MainActor // publish all UI changes back to main thread
 class AuthViewModel: ObservableObject {
     @Published var userSession : FirebaseAuth.User? // firebase user object
     @Published var currentUser : User?
     
     init() {
-        
+        // check if there is a local user logged in
+        // show landing view
+        self.userSession = Auth.auth().currentUser
     }
     
     func signIn(withEmail email : String, password : String) async throws {
@@ -23,12 +26,12 @@ class AuthViewModel: ObservableObject {
     
     func createUser(withEmail email : String, password : String, fullName : String) async throws{
         do {
-            // wait for user's result
+            // wait for result - creating a user with firebase
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
-            self.userSession = result.user
-            let user = User(id: result.user.uid, fullName: fullName, email: email) // customised user class
+            self.userSession = result.user // set user session
+            let user = User(id: result.user.uid, fullName: fullName, email: email) // customised user object
             let encodedUser = try Firestore.Encoder().encode(user)
-            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser) // uplode the user to firestore
         } catch {
             print("Failed to create user with error \(error.localizedDescription)")
         }
--- iOS-A3-Fitness Challenge/iOS_A3_Fitness_ChallengeApp.swift
+++ iOS-A3-Fitness Challenge/iOS_A3_Fitness_ChallengeApp.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct iOS_A3_Fitness_ChallengeApp: App {
     @StateObject var authViewModel = AuthViewModel() // initialise only once
     
+    // configure firebase
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
